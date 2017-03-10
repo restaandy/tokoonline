568f5ab1c1d435959160dd9bd19907a5945eff66
restaandy <restapradika@gmail.com>
2017-03-10 07:03:24 +0700

    coba

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin extends CI_Controller {
 
 	/**
-	 * Index Page for this controller.
+	 * Index Page for this controller..
 	 *
 	 * Maps to the following URL
 	 * 		http://example.com/index.php/welcome
